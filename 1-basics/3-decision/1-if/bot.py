7f21f2a675563d47bd86c57b84ddb93ab1833cbd
4opria68 <4opria68@solent.ac.uk>
2019-10-03 15:04:35 +0000

    Exercises completed for com404

---
+++ 1-basics/3-decision/1-if/bot.py
@@ -0,0 +1,6 @@
+print("What type of book is this?")
+book_type = input()
+
+if (book_type == "adventure"):
+    print("i like adventure books")
+print("Finish reading book")
---
+++ 1-basics/3-decision/2-if-else/bot.py
@@ -0,0 +1,8 @@
+print("Please enter the activity to be performed.")
+activity_type = input()
+if (activity_type =="calculate"):
+    print("performing calculation")
+else:
+    print("performing activity...")
+
+print("Activity completed")    
